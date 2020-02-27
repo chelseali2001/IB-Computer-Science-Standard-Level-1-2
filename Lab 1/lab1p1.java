42b7d38a3750407fba0290d743f92c5a871e181f
Chelsea Li <chelseali2001@gmail.com>
2020-02-27 11:04:50 -0800

    Create lab1p1.java

---
+++ Lab 1/lab1p1.java
@@ -0,0 +1,43 @@
+/**
+ * Shapes class
+ * @author Chelsea Li
+ */
+class lab1p1 {
+  /**
+   * Main method
+   * @param args
+   */
+  public static void main(String args[]) {
+    //Prints out a 3x3 rectangle
+    System.out.println("***");
+    System.out.println("***");
+    System.out.println("***");
+
+    //Prints out an oval
+    System.out.println("  *  ");
+    System.out.println(" * * ");
+    System.out.println("*   *");
+    System.out.println("*   *");
+    System.out.println("*   *");
+    System.out.println(" * * ");
+    System.out.println("  *  ");
+
+    //Prints out an up arrow
+    System.out.println("    *    ");
+    System.out.println("   ***   ");
+    System.out.println("  * * *  ");
+    System.out.println(" *  *  * ");
+    System.out.println("*   *   *");
+    System.out.println("    *    ");
+    System.out.println("    *    ");
+    System.out.println("    *    ");
+    System.out.println("    *    ");
+
+    //Prints out a rombus
+    System.out.println("  *  ");
+    System.out.println(" * * ");
+    System.out.println("*   *");
+    System.out.println(" * * ");
+    System.out.println("  *  ");
+  }
+}
